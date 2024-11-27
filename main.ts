6b57ac0366efc4ebdfa4a65796a93c9d32a917ae
HemanthSai K <khemanthsai01@gmail.com>
2024-11-27 13:53:03 +0530

    fix: errors

--- main.ts
+++ main.ts
@@ -3,39 +3,6 @@ import { getDailyReport } from "./daily_report.ts";
 import Slack from "@slack/bolt";
 import env from "./env.ts";
 
-const { newOrders, totalCredit, totalDebit, date } = await getDailyReport();
-
-const blocks = [
-  {
-    "type": "section",
-    "text": {
-      "type": "plain_text",
-      "text": "Hi :wave:",
-      "emoji": true,
-    },
-  },
-  {
-    "type": "section",
-    "text": {
-      "type": "mrkdwn",
-      "text": `here is dapp's notification for date: \`${date}\``,
-    },
-  },
-  {
-    "type": "section",
-    "text": {
-      "type": "mrkdwn",
-      "text": `New orders: \`${
-        newOrders ? newOrders : "0"
-      }\` \n Total credits user's added: \`${
-        totalCredit ? totalCredit : "0"
-      }\` \n Total bookings credit usage: \`${
-        totalDebit ? totalDebit : "0"
-      }\``,
-    },
-  },
-];
-
 const blocksTest = [
   {
     "type": "section",
@@ -47,14 +14,43 @@ const blocksTest = [
   },
 ];
 
-// console.log(blocks)
-
 const app = new Slack.App({
   signingSecret: env.SLACK_SIGNING_SECRET,
   token: env.SLACK_BOT_TOKEN,
 });
 
 Deno.cron("slack notification", "30 3 * * *", async () => {
+  const { newOrders, totalCredit, totalDebit, date } = await getDailyReport();
+  const blocks = [
+    {
+      "type": "section",
+      "text": {
+        "type": "plain_text",
+        "text": "Hi :wave:",
+        "emoji": true,
+      },
+    },
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": `here is dapp's notification for date: \`${date}\``,
+      },
+    },
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": `New orders: \`${
+          newOrders ? newOrders : "0"
+        }\` \n Total credits user's added: \`${
+          totalCredit ? totalCredit : "0"
+        }\` \n Total bookings credit usage: \`${
+          totalDebit ? totalDebit : "0"
+        }\``,
+      },
+    },
+  ];
   await app.client.chat.postMessage({
     text: "Hi :wave:",
     blocks,
