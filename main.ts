9fd3a04fc26e6ef58d5d6e6ac83b931282a96b0e
HemanthSai K <khemanthsai01@gmail.com>
2024-12-03 13:06:05 +0530

    fix: disable 6 hours cron

--- main.ts
+++ main.ts
@@ -42,35 +42,35 @@ Deno.cron("slack Daily notification", "30 3 * * *", async () => {
   });
 });
 
-Deno.cron("every 6 hours notification", "0 */6 * * *", async () => {
-  const { activeBookings } = await getActiveOrders();
-  const blocksTest = [
-    {
-      "type": "section",
-      "text": {
-        "type": "plain_text",
-        "text": "Hi :wave:",
-        "emoji": true,
-      },
-    },
-    {
-      "type": "section",
-      "text": {
-        "type": "mrkdwn",
-        "text": `here is dapp's notification`,
-      },
-    },
-    {
-      "type": "section",
-      "text": {
-        "type": "mrkdwn",
-        "text": `Active bookings: \`${activeBookings ? activeBookings : "0"}\``,
-      },
-    },
-  ];
-  await app.client.chat.postMessage({
-    text: "Hi :wave:",
-    blocks: blocksTest,
-    channel: env.SLACK_CHANEL!,
-  });
-});
+// Deno.cron("every 6 hours notification", "0 */6 * * *", async () => {
+//   const { activeBookings } = await getActiveOrders();
+//   const blocksTest = [
+//     {
+//       "type": "section",
+//       "text": {
+//         "type": "plain_text",
+//         "text": "Hi :wave:",
+//         "emoji": true,
+//       },
+//     },
+//     {
+//       "type": "section",
+//       "text": {
+//         "type": "mrkdwn",
+//         "text": `here is dapp's notification`,
+//       },
+//     },
+//     {
+//       "type": "section",
+//       "text": {
+//         "type": "mrkdwn",
+//         "text": `Active bookings: \`${activeBookings ? activeBookings : "0"}\``,
+//       },
+//     },
+//   ];
+//   await app.client.chat.postMessage({
+//     text: "Hi :wave:",
+//     blocks: blocksTest,
+//     channel: env.SLACK_CHANEL!,
+//   });
+// });
